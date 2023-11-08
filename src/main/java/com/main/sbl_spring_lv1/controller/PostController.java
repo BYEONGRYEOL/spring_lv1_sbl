6323c5bfbc4d4bd54e90f51d172d83df3d82a55a
BYEONGRYEOL <lyulbyung@gmail.com>
2023-11-08 20:23:33 +0900

    요청시 삭제된 게시글의 db id 를 반환받도록변경

--- src/main/java/com/main/sbl_spring_lv1/controller/PostController.java
+++ src/main/java/com/main/sbl_spring_lv1/controller/PostController.java
@@ -24,7 +24,7 @@ public class PostController {
     // controller의 역할은 적절한 url, 적절한 요청에 따른 처리 지시, 처리 결과 반환
     private final PostService postService; // final로 선언해서 RequiredArgsConstructor 로 생성자 포함
 
-    @GetMapping("/list")
+    @GetMapping("")
     @Operation(summary = "게시글 목록 조회", description = "모든 게시글 목록을 조회합니다.")
     @ApiResponse(responseCode = "200", description = "성공",
             content = {@Content(
@@ -73,7 +73,7 @@ public class PostController {
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "성공", content = {@Content(
                     mediaType = "text/plain",
-                    schema = @Schema(type = "number" , example = "1")
+                    schema = @Schema(type = "number", description = "삭제된 게시글 db id")
             )}),
             @ApiResponse(responseCode = "404", description = "실패 : 게시글이 존재하지 않음"),
             @ApiResponse(responseCode = "401", description = "실패 : 비밀번호 불일치")}
@@ -84,8 +84,8 @@ public class PostController {
             Long id,
             @RequestHeader String password
     ) {
-        postService.delete(id, password);
-        return id;
+        return postService.delete(id, password);
+
     }
 
 }
