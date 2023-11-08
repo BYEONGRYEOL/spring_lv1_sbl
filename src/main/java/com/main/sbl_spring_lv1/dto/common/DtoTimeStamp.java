273f5e6dea7b1cb269ad107e9daed907f2be30d1
BYEONGRYEOL <lyulbyung@gmail.com>
2023-11-08 16:46:05 +0900

    timestamp swagger schema doc 추가

--- src/main/java/com/main/sbl_spring_lv1/dto/common/DtoTimeStamp.java
+++ src/main/java/com/main/sbl_spring_lv1/dto/common/DtoTimeStamp.java
@@ -1,9 +1,12 @@
 package com.main.sbl_spring_lv1.dto.common;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import java.time.LocalDateTime;
 
 @Getter
 public abstract class DtoTimeStamp {
+    @Schema(description = "생성 날짜-시각")
     protected LocalDateTime createdAt;
+    @Schema(description = "수정 날짜-시각")
     protected LocalDateTime modifiedAt;
 }
