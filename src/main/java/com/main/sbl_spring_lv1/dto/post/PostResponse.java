902e008e67bf15da8a0246de8d3c752ae492fac2
BYEONGRYEOL <lyulbyung@gmail.com>
2023-11-08 16:45:01 +0900

    PostResponse swagger api doc 작성

--- src/main/java/com/main/sbl_spring_lv1/dto/post/PostResponse.java
+++ src/main/java/com/main/sbl_spring_lv1/dto/post/PostResponse.java
@@ -2,13 +2,19 @@ package com.main.sbl_spring_lv1.dto.post;
 
 import com.main.sbl_spring_lv1.dto.common.DtoTimeStamp;
 import com.main.sbl_spring_lv1.entity.Post;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 
 @Getter
+@Schema(description = "게시글 응답 스키마")
 public class PostResponse extends DtoTimeStamp {
+    @Schema(description = "게시글 db id")
     private Long id;
+    @Schema(description = "제목")
     private String title;
+    @Schema(description = "작성자 명")
     private String author;
+    @Schema(description = "작성 내용")
     private String contents;
 
     public PostResponse(Post post){
