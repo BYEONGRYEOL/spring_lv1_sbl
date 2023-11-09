32f2af3a6f7c4bc6ba61f704e4d223b20d81e2e0
BYEONGRYEOL <lyulbyung@gmail.com>
2023-11-09 22:01:03 +0900

    기본 예외 메세지 출력

--- src/main/java/com/main/sbl_spring_lv1/exception/post/PostPasswordNotMatchedException.java
+++ src/main/java/com/main/sbl_spring_lv1/exception/post/PostPasswordNotMatchedException.java
@@ -1,7 +1,7 @@
 package com.main.sbl_spring_lv1.exception.post;
 
-public class PostPasswordNotMatchedException extends RuntimeException{
+public class PostPasswordNotMatchedException extends RuntimeException {
     public PostPasswordNotMatchedException() {
-
+        super("게시글 비밀번호가 일치하지 않습니다.");
     }
 }
